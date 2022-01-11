5a3189b2c5cfb056045c2aef1ce167c08b560702
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 14:30:05 +0100

    detect the connected privider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import Web3 from 'web3'
+import Web3 from 'web3';
+//import '../build/contracts/Tenant.json'
 
-const web = new Web3(Web3.givenProvider)
+const web = new Web3(Web3.givenProvider || 'http://localhost:8545')
 
 
-console.log(web)
+const networkId = await web.eth.net.getId()
+
+console.log(networkId)
