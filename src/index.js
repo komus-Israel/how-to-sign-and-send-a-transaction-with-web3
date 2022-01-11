9eed00407748d18b209e3d7b9047c6b995a918c6
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 19:47:28 +0100

    environment variables working well

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Web3 from 'web3';
 import { createRequire } from 'module';
+import { config } from 'dotenv';
 
 
 // enable the use of require
 const require = createRequire(import.meta.url)
 const contractAbi = require("../build/contracts/Tenant.json")
+
+// set the config of the environment variable
+config()
 
 
 // initialize the web3 provider
@@ -17,6 +21,19 @@ const networkId = await web3.eth.net.getId()
 // get the contract address
 const address = contractAbi.networks[networkId].address 
 
+// get the public of the transaction signer
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+
+
 // direct web3 to the contract using the contract abi and the contract address
 const contract =  new web3.eth.Contract(contractAbi.abi, address)
-console.log(contract)
+
+
+//const setFirstTenant = await contract.methods.newTenant('Tom', 200).send({ from: PUBLIC_KEY })
+
+
+//const getFirstTenant = await contract.methods.tenants(1).call()
+//console.log(getFirstTenant)
+//console.log(setFirstTenant)
+console.log(PUBLIC_KEY)
+
---
+++ src/test.js
@@ -0,0 +1,8 @@
+//import { createRequire } from 'module';
+
+//const require = createRequire(import.meta.url)
+
+require('dotenv').config()
+
+const name = process.env.name 
+console.log(name)
