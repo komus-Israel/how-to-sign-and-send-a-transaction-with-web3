4e4621cb7b03c5733d322dd499f6d5eca59e321a
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 20:52:25 +0100

    created the transaction object

--- src/index.js
+++ src/index.js
@@ -19,21 +19,38 @@ const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
 const networkId = await web3.eth.net.getId()
 
 // get the contract address
-const address = contractAbi.networks[networkId].address 
+const contractAddress = contractAbi.networks[networkId].address 
 
 // get the public of the transaction signer
 const PUBLIC_KEY = process.env.PUBLIC_KEY
 
 
 // direct web3 to the contract using the contract abi and the contract address
-const contract =  new web3.eth.Contract(contractAbi.abi, address)
+const contract =  new web3.eth.Contract(contractAbi.abi, contractAddress)
 
 
-//const setFirstTenant = await contract.methods.newTenant('Tom', 200).send({ from: PUBLIC_KEY })
+//const registerTenant = await contract.methods.newTenant('Tom', 200).send({ from: PUBLIC_KEY })
 
 
-//const getFirstTenant = await contract.methods.tenants(1).call()
-//console.log(getFirstTenant)
-//console.log(setFirstTenant)
-console.log(PUBLIC_KEY)
+
+/**
+ * 
+ * creating new account
+ */
+
+/*const createAccount = web3.eth.accounts.create()
+const createdAccount = createAccount.address 
+const createdPrivateKey = createAccount.privateKey
+
+console.log("public key", createdAccount)
+console.log("private key", createdPrivateKey)*/
+
+const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    data: contract.methods.newTenant('Temi', 300).encodeABI()
+}
+
+
+
 
