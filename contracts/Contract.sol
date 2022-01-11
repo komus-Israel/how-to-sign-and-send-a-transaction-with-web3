de5eb47abf9550a8cb14e2cb4b7d4b8b6557dc62
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 12:42:57 +0100

    created function to add new tenant

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,20 +2,20 @@ pragma solidity 0.8.10;
 
 contract Tenant {
 
-    // variables to store tenant data
-
-    string public name;
-    string public age;
-    uint256 public price;
-
 
     // struct to store the tenant's data
     struct _Tenant {
         string name;
-        string age;
         uint256 price;
     }
 
-    map(uint256 => _Tenant);
+    uint256 counter;
+
+    map(uint256 => _Tenant) public tenants;
+
+    function newTenant(string memory name, uint256 price) public {
+        counter = counter + 1;
+        _Tenant _tenant = _Tenant(name, price);
+    }
 
 }
