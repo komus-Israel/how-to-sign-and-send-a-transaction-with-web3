dc710392636181dd75f2a0e4b3a9dc38d0ca5f3e
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 12:32:29 +0100

    created a  map to store the tenants data

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,8 +2,20 @@ pragma solidity 0.8.10;
 
 contract Tenant {
 
+    // variables to store tenant data
+
     string public name;
     string public age;
     uint256 public price;
+
+
+    // struct to store the tenant's data
+    struct _Tenant {
+        string name;
+        string age;
+        uint256 price;
+    }
+
+    map(uint256 => _Tenant);
 
 }
