cfec0c73dc3f502ce825d14de39c1fd792defbbb
komus-Israel <komolehinIsrael@gmail.com>
2022-01-11 14:42:56 +0100

    get the network id

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Web3 from 'web3';
 import { createRequire } from 'module';
 
+
+// enable the use of require
 const require = createRequire(import.meta.url)
-
-
 const contractAbi = require("../build/contracts/Tenant.json")
 
+
+// initialize the web3 provider
 const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
 
-console.log(contractAbi)
+
+// get the Id of the connected network
+const networkId = await web3.eth.net.getId()
+
+console.log(networkId)
